3b1a762bd0b589e391f4e70859024dc25dd791a1
Megan Bailey <meganbailey@Megans-MBP.lan1>
2019-03-01 22:49:45 -0800

    changed base classes

--- src/main/java/database/MongoDB.java
+++ src/main/java/database/MongoDB.java
@@ -1,23 +1,37 @@
 package database;
 
-import com.mongodb.*;
+//import com.mongodb.*;
+import com.mongodb.client.MongoDatabase; 
+import com.mongodb.MongoClient; 
+import com.mongodb.MongoCredential;
 
 public class MongoDB {
     private MongoClient mongoClient;
-    private DB database;
+    private MongoDatabase database;
     private String host;
     private String name;
     private int port;
+    private MongoCredential credential;
 
     /*
     *  public constructors
     */
+    public MongoDB() {
+        this.host = null;
+        this.port = 0;
+        this.name = null;
+        this.mongoClient = null;
+        this.database = null;
+        this.credential = null;
+    }
+
     public MongoDB(String host, int port) {
         this.host = host;
         this.port = port;
         this.name = null;
         this.mongoClient = null;
         this.database = null;
+        this.credential = null;
     }
 
     public MongoDB(String host, int port, String name) {
@@ -26,12 +40,23 @@ public class MongoDB {
         this.port = port;
         this.mongoClient = null;
         this.database = null;
+        this.credential = null;
+    }
+
+    public MongoDB(String host, int port, String name, String username, String password) {
+        this.host = host;
+        this.name = name;
+        this.port = port;
+        this.mongoClient = null;
+        this.database = null;
+        this.credential = MongoCredential.createCredential(username, this.name, password.toCharArray()); 
     }
 
     public void connect() throws DatabaseNotFoundException {
         if (this.name != null) {
-            this.mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:" + this.port));
-            DB database = mongoClient.getDB(this.name);
+            this.mongoClient =  new MongoClient( "localhost" , 27017 );
+            MongoDatabase database = mongoClient.getDatabase(this.name);
+            System.out.println("Connected to the database successfully");
         } else {
             throw new DatabaseNotFoundException("Database not found");
         } 
@@ -45,14 +70,25 @@ public class MongoDB {
         }
     }
 
-    public void auth(DB database, String username, String password) {
-        boolean auth = database.authenticate(username, password.toCharArray());
+    /*
+    public ListDatabasesIterable getDatabases() throws DatabaseNotFoundException {
+        if (this.mongoClient != null) {
+            return this.mongoClient.listDatabases();
+        } else {
+            throw new DatabaseNotFoundException("Database not found");
+        }
+        
     }
 
-    public void getDatabases() {
-        this.mongoClient.getDatabaseNames().forEach(System.out::println);
+    public void getCollections() throws DatabaseNotFoundException {
+        if (this.database != null) {
+            this.database.getCollectionNames().forEach(System.out::println);
+        } else {
+            throw new DatabaseNotFoundException("Database not found");
+        }
     }
-
+    */
+    
     public void setName(String newName) {
         this.name = newName;
     }
